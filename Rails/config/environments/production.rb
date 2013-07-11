b8f79bb0ce455ab8878b702e1b9380c04077502c
Bruce Ackerman <bcackerman@gmail.com>
2013-07-10 21:36:46 -0500

    middle work done for homepage

--- Rails/config/environments/production.rb
+++ Rails/config/environments/production.rb
@@ -16,6 +16,7 @@ Quid::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +47,7 @@ Quid::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( animate.css.scss application.css.scss boostrap-overrides.css.scss global.css.scss home.css.scss layout.css.scss mixins.css.scss application.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
